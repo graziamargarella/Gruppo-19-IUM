653ce7bde3e3c185f086ecfbb8691a5fedccedd0
wiscar01 <72261395+wiscar01@users.noreply.github.com>
2022-01-13 16:01:15 +0100

    add Login e Registrazione in index.html

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Fumetto per Registrazione/Login
+
+const loadTippy = document.getElementById('loadTippy()');
+	tippy('#header-icon', {
+  		content: loadTippy.innerHTML,
+  		placement: 'left',
+   		theme: 'light',
+   		trigger: 'click',
+   		interactive: true,
+   		allowHTML: true,
+});
